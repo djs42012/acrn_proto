087172bb32ed6b63f0434e661cc64037cec4f126
david <github@djs.gg>
2022-03-31 16:47:50 -0400

    Co-authored-by: mperfumo <mperfumo@users.noreply.github.com> Co-authored-by: KomodoTech <KomodoTech@users.noreply.github.com> Co-authored-by: micjun <micjun@users.noreply.github.com>

--- src/containers/NavigationContainer.tsx
+++ src/containers/NavigationContainer.tsx
@@ -3,15 +3,18 @@ import { View, ViewProps } from 'react-native';
 import ExternalUrl from '../components/ExternalUrl';
 // TODO: Create InternalLink Component
 
-export type url = [a: string, b: string]; //first index is page name, second is url
+export type url = [a: string, b: string]; // [0] -> page name, [1] -> url
 
-// * example for url type
-// const urls: url[] = [['a','b'], ['d', 'd']];
-// const url2: Array<url> = [['a','b'], ['c', 'd']];
-
+/*
+ * example for url type
+ * const urls: url[] = [['a','b'], ['d', 'd']];
+ * const url2: Array<url> = [['a','b'], ['c', 'd']];
+ */
 interface Props extends ViewProps {
   externalUrls: url[];
 }
+
+// TODO: Add unique keys to render
 
 function NavigationContainer(props: Props) {
   // * Maybe use hooks
